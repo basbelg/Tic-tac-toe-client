3ca364640a50186cf119c8064ae7dae469a1056a
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2020-04-04 01:16:19 -0700

    getInstance and getConnection added

--- src/sample/DBManager.java
+++ src/sample/DBManager.java
@@ -1,4 +1,27 @@
 package sample;
 
-public class DBManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager
+{
+    private static DBManager instance = new DBManager();
+
+    private DBManager() { }
+
+    public static DBManager getInstance()
+    {
+        return instance;
+    }
+
+    private static Connection getConnection()throws ClassNotFoundException, SQLException
+    {
+
+        Connection con = null;
+        Class.forName("com.mysql.jdbc.Driver");
+        con= DriverManager.getConnection();
+        return con;
+
+    }
 }

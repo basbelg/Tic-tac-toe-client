5f7cc8a9434b6c3766efe5c2f461f2c7b812ec0f
basbelg <basbeez@live.com>
2020-04-04 23:39:14 -0700

    Added an alternate constructor to Game class

--- src/DataClasses/Game.java
+++ src/DataClasses/Game.java
@@ -1,6 +1,7 @@
 package DataClasses;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class Game {
     private String id;
@@ -10,4 +11,16 @@ public class Game {
     private int player2Id;
     private int startingPlayerId;
     private int winningPlayerId;
+
+    public Game(Date startingTime, int player1Id, int player2Id, int startingPlayerId) {
+        id = UUID.randomUUID().toString();
+        this.startingTime = startingTime;
+        this.endTime = null;
+        this.player1Id = player1Id;
+        this.player2Id = player2Id;
+        this.startingPlayerId = startingPlayerId;
+        this.winningPlayerId = -1;
+    }
+
+    
 }

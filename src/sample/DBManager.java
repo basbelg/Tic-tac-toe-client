196c5bfef13893dbe124eca763ebc5680c58c6be
rbradt <rbradt1@saddleback.edu>
2020-04-05 13:09:51 -0700

    recieving and updating users

--- src/sample/DBManager.java
+++ src/sample/DBManager.java
@@ -1,38 +1,80 @@
 package sample;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import DataClasses.*;
+
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DBManager
 {
     private static DBManager instance = new DBManager();
 
-    Connection con = null;
+    private DBManager() {}
 
-    private DBManager() {
+    public static DBManager getInstance() {return instance;}
+
+    private static Connection getConnection() {
+        Connection connection = null;
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb1","root","<password>");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb1","root","<password>");
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-    }
 
-    public static DBManager getInstance()
-    {
-        return instance;
+        return connection;
     }
 
     public boolean addUser(User user) {
         boolean wasSuccessful = true;
+        Connection connection = getConnection();
+        PreparedStatement statement = null;
+
+        try {
+            statement = connection.prepareStatement("");
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(connection != null)
+                try {connection.close();} catch (SQLException e) {e.printStackTrace();}
+            if(statement != null)
+                try {statement.close();} catch (SQLException e) {e.printStackTrace();}
+        }
 
         return wasSuccessful;
     }
 
     public boolean updateUser(User user) {
         boolean wasSuccessful = true;
+        Connection connection = getConnection();
+        PreparedStatement statement = null;
+
+        try {
+            statement = connection.prepareStatement("update user set username = ?, password = ?, fname = ?, lname = ?, isActive = ? where id = ?");
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getFirstName());
+            statement.setString(4, user.getLastName());
+            statement.setBoolean(5, user.getIsActive());
+            statement.setInt(6, user.getId());
+
+            statement.executeUpdate();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(connection != null)
+                try {connection.close();} catch (SQLException e) {e.printStackTrace();}
+            if(statement != null)
+                try {statement.close();} catch (SQLException e) {e.printStackTrace();}
+        }
 
         return wasSuccessful;
     }
@@ -52,29 +94,48 @@ public class DBManager
         return wasSuccessful;
     }
 
-    public String getPassword(String username) {
-        String password = null;
-
-        return password;
-    }
-
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-
-        return users;
+        return getFilteredUsers("all");
     }
 
     public List<User> getFilteredUsers(String filter) {
         List<User> users = new ArrayList<>();
+        Connection connection = getConnection();
+        Statement statement = null;
 
-        return users;
-    }
-
-    public void close() {
         try {
-            con.close();
+            statement = connection.createStatement();
+            ResultSet resultSet;
+
+            switch (filter) {
+                case "active":
+                    resultSet = statement.executeQuery("select * from user where active=true");
+                    break;
+                case "deactive":
+                    resultSet = statement.executeQuery("select * from user where active=false");
+                    break;
+                case "all":
+                default:
+                    resultSet = statement.executeQuery("select * from user");
+            }
+
+            while(resultSet.next())
+                users.add(new User(resultSet.getInt("id"),
+                                    resultSet.getString("username"),
+                                    resultSet.getString("password"),
+                                    resultSet.getString("fname"),
+                                    resultSet.getString("lname"),
+                                    resultSet.getBoolean("isActive")));
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        finally {
+            if(connection != null)
+                try {connection.close();} catch (SQLException e) {e.printStackTrace();}
+            if(statement != null)
+                try {statement.close();} catch (SQLException e) {e.printStackTrace();}
+        }
+
+        return users;
     }
 }

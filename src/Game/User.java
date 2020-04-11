958d265261c040753ab52facc281dff3fc05349d
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2020-04-11 01:21:09 -0700

    Created Move class

--- src/Game/User.java
+++ src/Game/User.java
@@ -1,4 +1,44 @@
 package Game;
 
-public class User {
+public class User
+{
+    private String playerID;
+    private String userName;
+    private String firstName;
+    private String lastName;
+
+    public User(String playerID, String userName, String firstName, String lastName) {
+        this.playerID = playerID;
+        setUserName(userName);
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }

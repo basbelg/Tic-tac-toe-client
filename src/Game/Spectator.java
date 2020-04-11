bd92a0429c7425a606a6475b0a8825c58975ed89
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2020-04-11 16:04:54 -0700

    added attributes to spectator class

--- src/Game/Spectator.java
+++ src/Game/Spectator.java
@@ -1,4 +1,21 @@
 package Game;
 
-public class Spectator {
+public class Spectator
+{
+    private String gameID;
+    private String playerID;
+
+    public Spectator(String gameID, String playerID)
+    {
+        this.gameID = gameID;
+        this.playerID = playerID;
+    }
+
+    public String getGameID() {
+        return gameID;
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
 }

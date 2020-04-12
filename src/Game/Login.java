bb55986563d29ba2565a731c17bc83eecc6c3101
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2020-04-11 17:50:50 -0700

    made login UI

--- src/Game/Login.java
+++ src/Game/Login.java
@@ -1,4 +1,12 @@
 package Game;
 
-public class Login {
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Login
+{
+    public Button signInButton;
+    public Button newUserButton;
+    public TextField enterUsername;
+    public TextField enterPassword;
 }

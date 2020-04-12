810fec1ed093fa0e9204ba928904ae9df219e1c2
basbelg <basbeez@live.com>
2020-04-12 00:57:34 -0700

    Made it so that the user class implements Serializable

--- src/DataClasses/User.java
+++ src/DataClasses/User.java
@@ -1,6 +1,8 @@
 package DataClasses;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private int id;
     private String username;
     private String firstName;

a5c3e9400a2e9875a30765142a7ff85d62fc7771
basbelg <basbeez@live.com>
2020-04-13 01:48:17 -0700

    ConnectToLobbyMessage - Added an alternate constructor that accepts a lobbyGameId and player2 as parameters

--- Messages/src/Messages/ConnectToLobbyMessage.java
+++ Messages/src/Messages/ConnectToLobbyMessage.java
@@ -5,4 +5,9 @@ import DataClasses.User;
 public class ConnectToLobbyMessage {
     private String lobbyGameId;
     private User player2;
+
+    public ConnectToLobbyMessage(String lobbyGameId, User player2) {
+        this.lobbyGameId = lobbyGameId;
+        this.player2 = player2;
+    }
 }

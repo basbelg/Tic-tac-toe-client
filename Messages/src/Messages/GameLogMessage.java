ac4e32fe6ce4007bcb513c1d285815031f848899
basbelg <basbeez@live.com>
2020-04-13 15:16:25 -0700

    GameLogMessage - Added an alternate constructor that accepts a game id as a parameter

--- Messages/src/Messages/GameLogMessage.java
+++ Messages/src/Messages/GameLogMessage.java
@@ -3,4 +3,8 @@ package Messages;
 public class GameLogMessage {
     private String gameId;
     private List<Move> moveHistory;
+
+    public GameLogMessage(String gameId) {
+        this.gameId = gameId;
+    }
 }

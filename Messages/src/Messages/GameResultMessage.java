c47c04c537b64c4fe54e5f7034d49c10843658f0
basbelg <basbeez@live.com>
2020-04-13 15:26:39 -0700

    GameResultMessage - Added an alternate constructor that accepts a boolean as a parameter (set and sent by the server)

--- Messages/src/Messages/GameResultMessage.java
+++ Messages/src/Messages/GameResultMessage.java
@@ -2,4 +2,8 @@ package Messages;
 
 public class GameResultMessage {
     boolean isPlayer1Win;
+
+    public GameResultMessage(boolean isPlayer1Win) {
+        this.isPlayer1Win = isPlayer1Win;
+    }
 }

04d2789a85b6153e3471946b60bc67ba58f355df
basbelg <basbeez@live.com>
2020-04-13 14:23:29 -0700

    Added the Spectator class to the Main module

---
+++ src/DataClasses/Spectator.java
@@ -0,0 +1,13 @@
+package DataClasses;
+
+public class Spectator {
+    private String username;
+
+    public Spectator(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}

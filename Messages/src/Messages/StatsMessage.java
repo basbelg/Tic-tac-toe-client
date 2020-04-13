7b40f3eaf50ddf08e9218efdc95b9df061d59d8f
basbelg <basbeez@live.com>
2020-04-12 18:59:36 -0700

    StatsMessage - Added getters and setters for the data fields (the setter methods should only be used by the server)

--- Messages/src/Messages/StatsMessage.java
+++ Messages/src/Messages/StatsMessage.java
@@ -7,4 +7,28 @@ public class StatsMessage {
 
     public StatsMessage() {
     }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getTies() {
+        return ties;
+    }
+
+    public void setTies(int ties) {
+        this.ties = ties;
+    }
 }

099b24b80b59b9981114b0a9175dbad683535c49
basbelg <basbeez@live.com>
2020-04-13 02:05:46 -0700

    CreateAIGameMessage - Added an alternate constructor that accepts a Game object as a parameter

--- Messages/src/Messages/CreateAIGameMessage.java
+++ Messages/src/Messages/CreateAIGameMessage.java
@@ -3,5 +3,9 @@ package Messages;
 import GameInterfaces.Game;
 
 public class CreateAIGameMessage {
-    private Game newAIGame;
+    private Game newAIGame; // Player2 of this game will always be an AI player (whose ID is predefined to be 1)
+
+    public CreateAIGameMessage(Game newAIGame) {
+        this.newAIGame = newAIGame;
+    }
 }

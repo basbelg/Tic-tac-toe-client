6db6a2a3a10fbf3890d9f99d29596d102015be54
basbelg <basbeez@live.com>
2020-04-13 14:35:36 -0700

    GameViewersMessage - Added gameId: String and spectators: List<Spectator> data fields

--- Messages/src/Messages/GameViewersMessage.java
+++ Messages/src/Messages/GameViewersMessage.java
@@ -1,6 +1,12 @@
 package Messages;
 
+import DataClasses.Spectator;
+
+import java.util.List;
+
 public class GameViewersMessage {
     private String gameId;
+    private List<Spectator> spectators;
 
+    
 }

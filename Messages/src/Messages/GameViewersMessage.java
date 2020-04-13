000bdddd0ed0430ec256977920249fa658beeadf
basbelg <basbeez@live.com>
2020-04-13 14:59:45 -0700

    GameViewersMessage - Added an alternate constructor that accepts a game id as a parameter

--- Messages/src/Messages/GameViewersMessage.java
+++ Messages/src/Messages/GameViewersMessage.java
@@ -8,5 +8,7 @@ public class GameViewersMessage {
     private String gameId;
     private List<Spectator> spectators;
 
-    
+    public GameViewersMessage(String gameId) {
+        this.gameId = gameId;
+    }
 }

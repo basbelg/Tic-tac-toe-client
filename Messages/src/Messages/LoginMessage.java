52649c6bf6556176827617c0ae443f71cac72f29
basbelg <basbeez@live.com>
2020-04-12 22:16:39 -0700

    LoginMessage - Added a constructor that accepts both a username and password as parameters

--- Messages/src/Messages/LoginMessage.java
+++ Messages/src/Messages/LoginMessage.java
@@ -3,4 +3,9 @@ package Messages;
 public class LoginMessage {
     private String username;
     private String password;
+
+    public LoginMessage(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }

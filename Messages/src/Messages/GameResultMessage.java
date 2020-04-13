53f8890ef21f3f20e3f2ca6c00fafe653c07d917
basbelg <basbeez@live.com>
2020-04-13 15:28:39 -0700

    GameResultMessage - Added a toString method to alert users who won the game

--- Messages/src/Messages/GameResultMessage.java
+++ Messages/src/Messages/GameResultMessage.java
@@ -6,4 +6,9 @@ public class GameResultMessage {
     public GameResultMessage(boolean isPlayer1Win) {
         this.isPlayer1Win = isPlayer1Win;
     }
+
+    @Override
+    public String toString() {
+        return (isPlayer1Win ? "Player 1 Won the Game!" : "Player 2 Won the Game!");
+    }
 }

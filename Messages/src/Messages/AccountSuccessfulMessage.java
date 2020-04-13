f2e7d4bf96b1bb85838846b47c721dc5e7129ad5
basbelg <basbeez@live.com>
2020-04-12 23:14:09 -0700

    AccountSuccessfulMessage - Moved functionality to the LoginSuccessfulMessage class (the client must login after creating an account), and created a toString method that will inform the client if the account was created successfully

--- Messages/src/Messages/AccountSuccessfulMessage.java
+++ Messages/src/Messages/AccountSuccessfulMessage.java
@@ -3,13 +3,8 @@ package Messages;
 import DataClasses.User;
 
 public class AccountSuccessfulMessage {
-    private User createdUser;
-
-    public AccountSuccessfulMessage(User createdUser) {
-        this.createdUser = createdUser;
-    }
-
-    public User getCreatedUser() {
-        return createdUser;
+    @Override
+    public String toString() {
+        return "Account Created Successfully";
     }
 }

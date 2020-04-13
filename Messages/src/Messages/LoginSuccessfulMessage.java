a2764adeb3e5b8eebf91f0434520c5d48fb14f57
basbelg <basbeez@live.com>
2020-04-12 23:08:35 -0700

    LoginSuccessfulMessage - Moved the functionality of AccountSuccessfulMessage to LoginSuccessfulMessage

--- Messages/src/Messages/LoginSuccessfulMessage.java
+++ Messages/src/Messages/LoginSuccessfulMessage.java
@@ -1,4 +1,15 @@
 package Messages;
 
+import DataClasses.User;
+
 public class LoginSuccessfulMessage {
+    private User user;
+
+    public LoginSuccessfulMessage(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }

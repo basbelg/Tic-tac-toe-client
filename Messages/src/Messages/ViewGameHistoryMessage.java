85653fb9472f76117828b022fb4182ce6f5822e4
basbelg <basbeez@live.com>
2020-04-12 18:44:39 -0700

    ViewGameHistoryMessage - Added a getter and setter for the gameHistories data field (The setter method should only be used by the Server)

--- Messages/src/Messages/ViewGameHistoryMessage.java
+++ Messages/src/Messages/ViewGameHistoryMessage.java
@@ -5,4 +5,12 @@ public class ViewGameHistoryMessage {
 
     public ViewGameHistoryMessage() {
     }
+
+    public List<List<Move>> getGameHistories() {
+        return gameHistories;
+    }
+
+    public void setGameHistories(List<List<Move>> gameHistories) {
+        this.gameHistories = gameHistories;
+    }
 }

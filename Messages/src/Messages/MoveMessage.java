13bc8bca3e90b213af0009c66bb478d2b08f83ef
basbelg <basbeez@live.com>
2020-04-13 13:35:36 -0700

    MoveMessage - Added an alternate constructor

--- Messages/src/Messages/MoveMessage.java
+++ Messages/src/Messages/MoveMessage.java
@@ -4,4 +4,10 @@ public class MoveMessage {
     private Move nextMove;
     private String gameId;
     private int movingPlayerId;
+
+    public MoveMessage(Move nextMove, String gameId, int movingPlayerId) {
+        this.nextMove = nextMove;
+        this.gameId = gameId;
+        this.movingPlayerId = movingPlayerId;
+    }
 }

44657c7762a39993e611e6a0ead0f5402ba482cf
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2020-04-15 19:13:55 -0700

    Created Board UI and added Gridpane

---
+++ src/Controllers/BoardController.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BoardController implements Initializable
+{
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+}

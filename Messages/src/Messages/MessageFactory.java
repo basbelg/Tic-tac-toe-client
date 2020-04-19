edeb170d700088cbdd522347d73346d3a2d7f259
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2020-04-18 18:50:30 -0700

    Message factory compeleted

--- Messages/src/Messages/MessageFactory.java
+++ Messages/src/Messages/MessageFactory.java
@@ -1,12 +1,55 @@
 package Messages;
 
 public final class MessageFactory {
-    private MessageFactory() {
-    }
+    private MessageFactory() {}
 
-    public static Object getMessage(String type) {
-        switch(type) {
+    public static Object getMessage(String type)
+    {
+        switch(type)
+        {
+            case "ACF-MSG": return new AccountFailedMessage();
 
+            case "ACS-MSG": return new AccountSuccessfulMessage();
+
+            case "CNT-MSG": return new ConnectToLobbyMessage();
+
+            case "CAC-MSG": return new CreateAccountMessage();
+
+            case "CAI-MSG": return new CreateAIGameMessage();
+
+            case "CLB-MSG": return new CreateLobbyMessage();
+
+            case "DAC-MSG": return new DeactivateAccountMessage();
+
+            case "FUL-MSG": return new FullLobbyMessage();
+
+            case "GLG-MSG": return new GameLogMessage();
+
+            case "GRE-MSG": return new GameResultMessage();
+
+            case "GVW-MSG": return new GameViewersMessage();
+
+            case "ILM-MSG": return new IllegalMoveMessage();
+
+            case "IAG-MSG": return new InactiveGameMessage();
+
+            case "LEM-MSG": return new LegalMoveMessage();
+
+            case "LOF-MSG": return new LoginFailedMessage();
+
+            case "LOG-MSG": return new LoginMessage();
+
+            case "LOS-MSG": return new LoginSuccessfulMessage();
+
+            case "MOV-MSG": return new MoveMessage();
+
+            case "NAI-MSG": return new NewAILobbyMessage();
+
+            case "NLB-MSG": return new NewLobbyMessage();
+
+            case "SPC-MSG": return new SpectateMessage();
+
+            case "UPA-MSG": return new UpdateAccountInfoMessage();
         }
 
         return null;

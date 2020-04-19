11502936bead8ebda4abcce6e91f154f273714a0
basbelg <basbeez@live.com>
2020-04-18 17:00:44 -0700

    Changed the GameResultMessage to only have a String data field (the username of the winner), which should be null if there is a tie

--- Messages/src/Messages/GameResultMessage.java
+++ Messages/src/Messages/GameResultMessage.java
@@ -3,23 +3,17 @@ package Messages;
 import java.io.Serializable;
 
 public class GameResultMessage implements Serializable {
-    boolean isPlayer1Win;
-    boolean isPlayer2Win;
+    String winner;
 
     public GameResultMessage() {
     }
 
-    public void setPlayer1Win(boolean player1Win) {
-        isPlayer1Win = player1Win;
-    }
-
-    public void setPlayer2Win(boolean player2Win) {
-        isPlayer2Win = player2Win;
+    public void setWinner(String winner) {
+        this.winner = winner;
     }
 
     @Override
     public String toString() {
-        return (isPlayer1Win ? "Player 1 Won the Game!" :
-               (isPlayer2Win ? "Player 2 Won the Game!" : "It's a Tie!"));
+        return (winner != null ? (winner + " has won the game") : "It's a tie!");
     }
 }

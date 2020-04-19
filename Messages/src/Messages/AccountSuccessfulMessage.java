5a93778c156c6a7be8bef3ed347aa9077738db85
basbelg <basbeez@live.com>
2020-04-18 22:52:55 -0700

    Deleted User data field in AccountSuccessfulMessage

--- Messages/src/Messages/AccountSuccessfulMessage.java
+++ Messages/src/Messages/AccountSuccessfulMessage.java
@@ -5,17 +5,8 @@ import DataClasses.User;
 import java.io.Serializable;
 
 public class AccountSuccessfulMessage implements Serializable {
-    private User newAccount;
 
     public AccountSuccessfulMessage() {
-    }
-
-    public User getNewAccount() {
-        return newAccount;
-    }
-
-    public void setNewAccount(User newAccount) {
-        this.newAccount = newAccount;
     }
 
     @Override
